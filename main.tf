0e0389b00a95bc5a3babfadc35cc66f54f049322
vaishnavipadwal <vaishupadwal12@gmail.com>
2025-03-18 22:11:51 +0530

    creating EKS using terraform

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.0"
+#     }
+#   }
+# }
 
 # Create VPC
 module "vpc" {
