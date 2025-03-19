f31eae2a08d0187c47b61677a5a636c512ca3a6c
vaishnavipadwal <vaishupadwal12@gmail.com>
2025-03-19 20:09:34 +0530

    creating EKS using terraform

--- outputs.tf
+++ outputs.tf
@@ -20,7 +20,7 @@ output "node_group_arn" {
 
 output "node_group_name" {
   description = "Name of the EKS managed node group"
-  value = module.eks.eks_managed_node_groups["eks_nodes"].id
+  value = module.eks.eks_managed_node_groups["eks_nodes"].arn
 
 
 }
