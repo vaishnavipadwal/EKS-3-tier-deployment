885f27fe78f398bd343da3d9d9c045307764c8cb
vaishnavipadwal <vaishupadwal12@gmail.com>
2025-03-24 17:08:19 +0530

    creating eks-3-tier-project

--- terraform/eks-workers.tf
+++ terraform/eks-workers.tf
@@ -2,7 +2,7 @@ resource "aws_eks_node_group" "workers" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "eks-worker-group"
   node_role_arn   = aws_iam_role.eks_worker_role.arn  # Ensure this reference is correct
-  subnet_ids      = aws_subnet.private_subnets[*].id
+  subnet_ids      = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
   instance_types  = ["t3.medium"]
 
   scaling_config {
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,15 +1,42 @@
 resource "aws_vpc" "eks_vpc" {
   cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "eks-vpc"
+  }
 }
 
 resource "aws_subnet" "private_subnet_1" {
-  vpc_id            = aws_vpc.eks_vpc.id
-  cidr_block        = var.subnet_cidrs[0]
-  availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = var.subnet_cidrs[0]
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "private-subnet-1"
+  }
 }
 
 resource "aws_subnet" "private_subnet_2" {
-  vpc_id            = aws_vpc.eks_vpc.id
-  cidr_block        = var.subnet_cidrs[1]
-  availability_zone = "us-east-1b"
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = var.subnet_cidrs[1]
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "private-subnet-2"
+  }
+}
+
+# List of private subnets for easy reference in eks-workers.tf
+resource "aws_subnet" "private_subnets" {
+  count                   = 2
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = var.subnet_cidrs[count.index]
+  availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "private-subnet-${count.index + 1}"
+  }
 }
