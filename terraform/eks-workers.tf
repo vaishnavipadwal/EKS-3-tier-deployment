6e8f116b424e3951954a93837b7f268d2d1ec3b9
vaishnavipadwal <vaishupadwal12@gmail.com>
2025-03-24 17:03:58 +0530

    creating eks-3-tier-project

--- terraform/eks-workers.tf
+++ terraform/eks-workers.tf
@@ -1,11 +1,13 @@
 resource "aws_eks_node_group" "workers" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
-  node_role_arn   = aws_iam_role.eks_worker_role.arn
-  subnet_ids      = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
+  node_group_name = "eks-worker-group"
+  node_role_arn   = aws_iam_role.eks_worker_role.arn  # Ensure this reference is correct
+  subnet_ids      = aws_subnet.private_subnets[*].id
   instance_types  = ["t3.medium"]
+
   scaling_config {
     desired_size = 2
-    min_size     = 1
     max_size     = 3
+    min_size     = 1
   }
 }
--- terraform/iam.tf
+++ terraform/iam.tf
@@ -17,3 +17,33 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
+
+resource "aws_iam_role" "eks_worker_role" {
+  name = "eks-worker-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_worker_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_worker_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_worker_role.name
+}
